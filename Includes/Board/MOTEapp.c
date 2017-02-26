3fe8f75eba644abef3fddc4e8b60f7d994a5e5e1
glederer007 <gabor@lederer.hu>
2017-02-26 18:04:35 +0100

    Second version. Added burts uplink.

--- Includes/Board/MOTEapp.c
+++ Includes/Board/MOTEapp.c
@@ -129,6 +129,9 @@ static uint8_t sleepTicker = 0;
 typedef enum {rn2483, rn2903} RN_T;
 static RN_T rnModule = rn2483;
 
+// sendingCounter long
+static long sendingCounter = 0; // this counter will be included for all uplink messages
+
 void moduleResync(void)
 {
     bool escapeEvent = false;
@@ -580,9 +583,9 @@ static MOTE_RUNNING_T moteRunningProcess(bool changeStates, bool selectButton, b
                 // Prepare Display
                 oled_clear();
                 oled_putString("Uplink Pckt Type",0,0);
-                oled_putString("Throu Port# ",0,1);
-                
-                oled_putString("(UNCNF)    (CNF) ",0,3);
+//                oled_putString("Throu Port# ",0,1);
+//                oled_putString("Throu Port# ",0,1);
+                oled_putString("(UNCNF)   (Burst) ",0,3);
                 // Prepare Buffers and data
                 moteApp_clearBuffers();
                 randomPortNum = TMR2_ReadTimer();
@@ -622,7 +625,19 @@ static MOTE_RUNNING_T moteRunningProcess(bool changeStates, bool selectButton, b
             }
             if (selectButton)
             {
-                sendDataCommand("mac tx cnf ", dataBuffer, 12);
+                // send a burst of five uplink messages
+                int ctr;
+	
+                /* for loop execution */
+                moteApp_clearBuffers();
+                moteApp_add16bToDataBuffer(sendingCounter, 3);
+                for( ctr = 0; ctr < 5; ctr = ctr + 1 ){
+                    moteApp_add8bToDataBuffer(ctr, 9);
+                    
+                    sendDataCommand("mac tx uncnf ", dataBuffer, 12);
+                }
+                sendingCounter++;
+                
             }
 
             if ( (changeStates) || (selectButton) )
---
+++ LoRaMote.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Standalone configuration
+CND_ARTIFACT_DIR_Standalone=dist/Standalone/production
+CND_ARTIFACT_NAME_Standalone=LoRaMote.X.production.hex
+CND_ARTIFACT_PATH_Standalone=dist/Standalone/production/LoRaMote.X.production.hex
+CND_PACKAGE_DIR_Standalone=${CND_DISTDIR}/Standalone/package
+CND_PACKAGE_NAME_Standalone=loramote.x.tar
+CND_PACKAGE_PATH_Standalone=${CND_DISTDIR}/Standalone/package/loramote.x.tar
+# Offset configuration
+CND_ARTIFACT_DIR_Offset=dist/Offset/production
+CND_ARTIFACT_NAME_Offset=LoRaMote.X.production.hex
+CND_ARTIFACT_PATH_Offset=dist/Offset/production/LoRaMote.X.production.hex
+CND_PACKAGE_DIR_Offset=${CND_DISTDIR}/Offset/package
+CND_PACKAGE_NAME_Offset=loramote.x.tar
+CND_PACKAGE_PATH_Offset=${CND_DISTDIR}/Offset/package/loramote.x.tar
+# Combined configuration
+CND_ARTIFACT_DIR_Combined=dist/Combined/production
+CND_ARTIFACT_NAME_Combined=LoRaMote.X.production.hex
+CND_ARTIFACT_PATH_Combined=dist/Combined/production/LoRaMote.X.production.hex
+CND_PACKAGE_DIR_Combined=${CND_DISTDIR}/Combined/package
+CND_PACKAGE_NAME_Combined=loramote.x.tar
+CND_PACKAGE_PATH_Combined=${CND_DISTDIR}/Combined/package/loramote.x.tar
